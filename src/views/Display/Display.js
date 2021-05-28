781c0e66ddeb39b0cdd087d09ba138487ffa35b5
skuiry <57824978+shnackquery@users.noreply.github.com>
2021-05-28 11:13:37 -0700

    Fixed recipe display based on new attributes

--- src/views/Display/Display.js
+++ src/views/Display/Display.js
@@ -2,16 +2,17 @@ import React from 'react';
 // @material-ui components
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import Shrump from "../../img/shrimp-paella.jpg";
+import Shrimp from "../../img/shrimp-paella.jpg";
 import '../../css/Display.css';
 // Components
 
 //Using local storage for now, working on using props
 let info = JSON.parse(localStorage.getItem("data"));
 
+
 export default function Display() {
     const comment = React.useRef(null);
-
+    console.log(localStorage.getItem("data"));
     const handleSubmit = e => {
     console.log("Comment added: " + comment.current.value);
     e.preventDefault();
@@ -23,7 +24,7 @@ export default function Display() {
             <Grid container direction="row" justify='center' spacing={1}>
               <Grid item xs={1}></Grid>
               <Grid item xs={9}>
-                <b className="recipe-name">~{info.name}~</b>
+                <b className="recipe-name">~{info.title}~</b>
               </Grid>
               <Grid item xs={1}>
               </Grid>
@@ -35,7 +36,7 @@ export default function Display() {
             <Grid container direction="column" justify="left" spacing={3}>
               <Grid container direction="row" spacing={9}>
                 <Grid item xs={5}>
-                    <img  src={Shrump}></img>
+                    <img  src={Shrimp}></img>
                   </Grid>
                 <Grid item xs={5}>
                     <Grid container direction="column" justify="left" spacing={5}>
@@ -46,7 +47,13 @@ export default function Display() {
                             <div><b>Date Uploaded: </b> {info.date}</div>
                         </Grid>
                         <Grid item xs={9}>
+                            <div><b>Cuisine: </b> {info.cuisine}</div>
+                        </Grid>
+                        <Grid item xs={9}>
                             <div><b>Description:</b> {info.description}</div>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <div><b>Ingredients:</b> {info.ingredients}</div>
                         </Grid>
                         <Grid item xs={9}>
                             <div><b>Recipe:</b> {info.recipe}</div>
