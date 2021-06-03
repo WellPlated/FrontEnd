c033f9fb1bd4d4156d26d2cdd19ed5cf0092ea2d
Michael <mlsimon734@gmail.com>
2021-06-03 15:13:14 -0700

    removed extraneous stuff

--- src/views/SideNav/SideNav.js
+++ src/views/SideNav/SideNav.js
@@ -1,16 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Divider from "@material-ui/core/Divider";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import MenuIcon from "@material-ui/icons/Menu";
-import CloseIcon from "@material-ui/icons/Close";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import '../../css/SideNav.css';
 
@@ -75,8 +65,6 @@ const SideNav = (props) => {
   );
 };
 export default SideNav;
-
-
 
 
 SideNav.prototypes = {
